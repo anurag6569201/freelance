7bbae438420415fb9c47ac2c840c2e3a7394dd25
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2025-01-27 09:54:13 +0530

    minor changes

--- asian-adept/assets/js/custom.js
+++ asian-adept/assets/js/custom.js
@@ -426,7 +426,28 @@
     });
     // event-expertise-carousel - end
     // --------------------------------------------------
-  
+    $(document).ready(function () {
+      $(".owl-carousel_images").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 3
+          },
+          1000: {
+            items: 6
+          }
+        }
+      });
+    });
   
   
   
