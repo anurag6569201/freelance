ee30267441afbcab0c1e70fe12e510bf694680a2
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2025-02-03 00:41:39 +0530

    updated full django codes

--- asian/home/urls.py
+++ asian/home/urls.py
@@ -10,4 +10,11 @@ urlpatterns=[
 
     path('service/corporate',views.corp_service,name='corp_service'),
     path('service/social',views.corp_social,name='corp_social'),
+    path('service/exhibition',views.corp_exhibition,name='corp_exhibition'),
+    path('service/event',views.corp_event,name='corp_event'),
+
+    path('gallery/',views.gallery,name='gallery'),
+    
+    path('career/',views.career,name='career'),
+    path('contact/',views.contact,name='contact'),
 ]
--- asian/home/views.py
+++ asian/home/views.py
@@ -14,16 +14,57 @@ def team(request):
 
 
 def corp_service(request):
+    title = "Corporate"
     corporate = models.CorporateSerices.objects.all()
     context={
         'corporate':corporate,
+        'title':title,
     }
     return render(request,"pages/services/corporate.html",context)
 
 
 def corp_social(request):
+    title = "Social"
     social = models.SocialSerices.objects.all()
     context={
         'social':social,
+        'title':title,
     }
     return render(request,"pages/services/social.html",context)
+
+def corp_exhibition(request):
+    title = "Exhibitions"
+    exhibition = models.ExhibitionSerices.objects.all()
+    context={
+        'exhibition':exhibition,
+        'title':title,
+    }
+    return render(request,"pages/services/exhibition.html",context)
+
+
+def corp_event(request):
+    title = "Events"
+    event = models.EventSerices.objects.all()
+    context={
+        'event':event,
+        'title':title,
+    }
+    return render(request,"pages/services/event.html",context)
+
+
+
+
+
+def gallery(request):
+    return render(request,"pages/gallery/gallery.html")
+
+
+
+def career(request):
+    return render(request,"pages/career/career.html")
+
+
+
+def contact(request):
+    return render(request,"pages/contact/contact.html")
+
